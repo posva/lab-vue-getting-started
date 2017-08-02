300a9a0dfa8457564179fea550fb1d00f022ceaf
Eduardo San Martin Morote <posva13@gmail.com>
2017-08-02 19:47:20 +0200

    Use Mocha for testing

--- vue-simple/hello-world/test.js
+++ vue-simple/hello-world/test.js
@@ -1,6 +1,43 @@
-Feature('Hello Vue');
+window.onload = () => {
+  // create a div where mocha can add its stuff
+  const mochaDiv = document.createElement('DIV')
+  mochaDiv.id = 'mocha'
+  document.body.appendChild(mochaDiv)
 
-Scenario('test something', (I) => {
-  I.amOnPage('/')
-  I.see('Hello Vue')
-})
+  // window.should()
+  // window.chai.use(chaiDom)
+  window.should = chai.should()
+  window.mocha.ui('bdd')
+
+  const emptyNodes = document.querySelectorAll('nonexistant')
+  const app = document.querySelector('#app')
+  window.$$ = function $$ (selector) {
+    const els = document.querySelectorAll(selector)
+    const innerEls = app.querySelectorAll(selector)
+    const fn = innerEls.length
+            ? el => innerEls.find(el)
+            : el => app === el
+    const found = Array.from(els).filter(fn)
+    return found.length
+      ? found
+      : emptyNodes
+  }
+
+  window.$ = function $ (selector) {
+    const els = document.querySelectorAll(selector)
+    const innerEl = app.querySelector(selector)
+    const fn = innerEl
+            ? el => el === innerEl
+            : el => el === app
+    // Allow should chaining for tests
+    return Array.from(els).find(fn) || emptyNodes
+  }
+
+  describe('Hello World', () => {
+    it('display Hello Vue', () => {
+      $('p').should.have.text('Hello Vue')
+    })
+  })
+
+  window.mocha.run()
+}
--- vue-simple/run.sh
+++ vue-simple/run.sh
@@ -6,12 +6,4 @@ if [ -s debug.sh ]; then
     source debug.sh
     echo '--- end of debug.sh ---'
 fi
-echo "TECHIO> open -p 5000 --static-dir /project/target/$1 /index.html"
-
-# ./node_modules/.bin/serve -p 5000 "$1" &
-# SPID="$!"
-# sleep 1
-
-curl 'http://localhost:5000'
-# kill "$SPID"
-# ./node_modules/.bin/codeceptjs run --steps -o "{\"tests\": \"$1/test.js\"}"
+echo "TECHIO> open --static-dir /project/target/$1 /index.html"
