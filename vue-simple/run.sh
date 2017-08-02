45f97ab2cfacbd76ff5d6d406fe042b1e5cc6320
Eduardo San Martin Morote <posva13@gmail.com>
2017-08-02 15:53:04 +0200

    Hide debug.sh

--- vue-simple/run.sh
+++ vue-simple/run.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 echo "🚀 Let's go with $1!"
-pwd
-ls
-echo '---'
-source debug.sh
-echo '---'
+if [ ! -s debug.sh ]; then
+    echo '--- 🐛 sourcing debug.sh ---'
+    source debug.sh
+    echo '--- end of debug.sh ---'
+fi
 echo "TECHIO> open --static-dir /project/target/$1 /index.html"
