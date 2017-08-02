adfa2b31ef80c368875e600f95f1f325a893097c
Eduardo San Martin Morote <posva13@gmail.com>
2017-08-02 19:20:28 +0200

    Testing more

--- vue-simple/run.sh
+++ vue-simple/run.sh
@@ -6,12 +6,12 @@ if [ -s debug.sh ]; then
     source debug.sh
     echo '--- end of debug.sh ---'
 fi
-echo "TECHIO> open 5000 --static-dir /project/target/$1 /index.html"
+echo "TECHIO> open -p 5000 --static-dir /project/target/$1 /index.html"
 
-./node_modules/.bin/serve -p 5000 "$1" &
-SPID="$!"
-sleep 1
+# ./node_modules/.bin/serve -p 5000 "$1" &
+# SPID="$!"
+# sleep 1
 
 curl 'http://localhost:5000'
-kill "$SPID"
+# kill "$SPID"
 # ./node_modules/.bin/codeceptjs run --steps -o "{\"tests\": \"$1/test.js\"}"
