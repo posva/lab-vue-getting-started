cb18c68f6b8e2f0288cc70dfeba68c19bc639e90
Eduardo San Martin Morote <posva13@gmail.com>
2017-08-05 20:19:34 +0200

    Lets Test!

--- vue-simple/run.sh
+++ vue-simple/run.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 echo "🚀 Let's go with $1!"
+cd "$1" && ../node_modules/.bin/webpack -d --progress --config=../webpack.config.js ; cd ..
+cd "$1" && ../node_modules/.bin/karma start ../karma.conf.js --single-run; cd ..
 if [ -s debug.sh ]; then
     echo '--- 🐛 sourcing debug.sh ---'
     source debug.sh
     echo '--- end of debug.sh ---'
 fi
-echo "TECHIO> open --static-dir /project/target/$1 /index.html"
+echo "TECHIO> open --static-dir /project/target/$1/dist /index.html"
