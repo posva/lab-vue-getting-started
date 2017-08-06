d280dcb465b986398ebd55884f69b2749a638d30
mc100s <mc100s.b@gmail.com>
2017-08-06 23:02:48 +0200

    Test on open.sh

--- vue-simple/open.sh
+++ vue-simple/open.sh
@@ -1,1 +1,19 @@
-echo "TECHIO> open -s /project/target/$1 index.html"
+#!/bin/bash
+
+echo "🚀 Let's go with $1!"
+cd "$1" && ../node_modules/.bin/webpack -d --progress --config=../webpack.config.js ; cd ..
+echo "TECHIO> open --static-dir /project/target/$1/dist /index.html"
+if cd "$1" && ../node_modules/.bin/karma start ../karma.conf.js --single-run; then
+    GOOD=true
+else
+    GOOD=false
+fi
+
+cd ..
+echo "TECHIO> success $GOOD"
+
+if [ -s debug.sh ]; then
+    echo '--- 🐛 sourcing debug.sh ---'
+    source debug.sh
+    echo '--- end of debug.sh ---'
+fi
