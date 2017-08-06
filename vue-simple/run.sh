b335251e8bf995183f49cfa9f22f3e2c000976ac
Eduardo San Martin Morote <posva13@gmail.com>
2017-08-06 14:06:01 +0200

    Use node-chrome

--- vue-simple/run.sh
+++ vue-simple/run.sh
@@ -2,10 +2,18 @@
 
 echo "🚀 Let's go with $1!"
 cd "$1" && ../node_modules/.bin/webpack -d --progress --config=../webpack.config.js ; cd ..
-cd "$1" && ../node_modules/.bin/karma start ../karma.conf.js --single-run; cd ..
+echo "TECHIO> open --static-dir /project/target/$1/dist /index.html"
+if cd "$1" && ../node_modules/.bin/karma start ../karma.conf.js --single-run; then
+    GOOD=true
+else
+    GOOD=false
+fi
+
+cd ..
+echo "TECHIO> success $GOOD"
+
 if [ -s debug.sh ]; then
     echo '--- 🐛 sourcing debug.sh ---'
     source debug.sh
     echo '--- end of debug.sh ---'
 fi
-echo "TECHIO> open --static-dir /project/target/$1/dist /index.html"
